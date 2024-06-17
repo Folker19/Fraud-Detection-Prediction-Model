58945a6c74453925489b19b1ee20d2d1c3bdc7fa
Carrillo1992 <danielcarrillo1992@gmail.com>
2024-06-17 20:18:11 +0200

    Update Streamlit_FraudDetection.py

--- Streamlit_FraudDetection.py
+++ Streamlit_FraudDetection.py
@@ -26,6 +26,8 @@ def load_data(path):
     return df
 
 # Load the dataset raw
+with zp.ZipFile('data/transactions.part1.rar', 'r') as zip_ref:
+    zip_ref.extractall('data/')
 data_raw_path = 'data/transactions.csv'
 df_raw = load_data(data_raw_path)
 # Load the dataset processed
@@ -49,9 +51,17 @@ options = st.sidebar.radio('Select an option:', ['Introduction', 'EDA & CDA', 'M
 # Show the dataset
 if options == 'Introduction':
     st.header('Introduction')
+<<<<<<< Updated upstream
     st.write(df_raw.head())
     st.write(df_raw.shape)
     st.write('Credits: https://www.kaggle.com/datasets/shriyashjagtap/fraudulent-e-commerce-transactions/data)')
+=======
+
+    st.write(df_raw.head())
+    st.write(df_raw.shape)
+    st.write('Credits: https://www.kaggle.com/datasets/shriyashjagtap/fraudulent-e-commerce-transactions/data)')
+
+>>>>>>> Stashed changes
     #########
     st.markdown('***Description:***')
     st.write('This synthetic dataset, "transactions," has been generate with Pythons Faker library to simulate transaction data from an e-commerce platform with a focus on fraud detection. It includes a range of features commonly found in transactional data, along with additional attributes specifically designed to support the development and testing of fraud detection algorithms.')
@@ -60,6 +70,10 @@ if options == 'Introduction':
     st.markdown('***Objective:***')
     st.write('The objective of this project is to develop a machine learning model that can predict whether a transaction is fraudulent or not. The model has been trained on the mentioned dataset which labels transactions as fraudulent or legitimate.')
     st.write('Size of the dataset: 1,472,592 rows and 16 columns')
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 # EDA
 elif options == 'EDA & CDA':
